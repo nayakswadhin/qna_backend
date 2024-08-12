afbd8c78cdca31d389bf168f4207103f978a1a29
Swadhin Kumar Nayak <nayak.swadhin25@gmail.com>
2024-08-13 01:23:39 +0530

    added update feature

--- index.js
+++ index.js
@@ -22,16 +22,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/question", (req, res) => {
-  const q = "SELECT * FROM bzr1y9jplzahkedywbh7.qa";
+  const q = "SELECT * FROM question.qa";
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
 });
 
+app.get("/question/:id", (req, res) => {
+  const quesid = req.params.id;
+  const q = "SELECT * FROM question.qa WHERE id = ?";
+  db.query(q, [quesid], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 app.post("/question", (req, res) => {
-  const q =
-    "INSERT INTO `bzr1y9jplzahkedywbh7`.`qa` (`question`, `answer`) VALUES (?)";
+  const q = "INSERT INTO `question`.`qa` (`question`, `answer`) VALUES (?)";
   const values = [req.body.question, req.body.answer];
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
@@ -40,15 +48,29 @@ app.post("/question", (req, res) => {
 });
 
 app.delete("/question/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = "DELETE FROM `bzr1y9jplzahkedywbh7`.`qa` WHERE id = ?";
+  const quesid = req.params.id;
+  const q = "DELETE FROM `question`.`qa` WHERE id = ?";
 
-  db.query(q, [bookId], (err, data) => {
+  db.query(q, [quesid], (err, data) => {
     if (err) return res.json(err);
     return res.json("Book has been deleted sucessfully");
+  });
+});
+
+app.put("/question/:id", (req, res) => {
+  const quesid = req.params.id;
+  const q = "UPDATE question.qa SET `question` = ?, `answer` = ? WHERE id = ?";
+
+  const values = [req.body.question, req.body.answer];
+
+  db.query(q, [...values, quesid], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated sucessfully");
   });
 });
 
 app.listen(8080, () => {
   console.log("Listening to backend");
 });
+
+//bzr1y9jplzahkedywbh7
